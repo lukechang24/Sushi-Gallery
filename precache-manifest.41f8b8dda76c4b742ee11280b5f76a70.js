fe4a8c946ea64dfeed1eb79451e3aa1d340a1272
lukechang24 <lukechang244@gmail.com>
2020-02-14 14:15:21 -0800

    Updates

---
+++ precache-manifest.41f8b8dda76c4b742ee11280b5f76a70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a421b06c052a743d6f35d410ed410c",
+    "url": "/Sushi-Gallery/index.html"
+  },
+  {
+    "revision": "3fc5b0afaa8077e6c57b",
+    "url": "/Sushi-Gallery/static/css/main.a1bd2ab3.chunk.css"
+  },
+  {
+    "revision": "024237a2e7a249aed019",
+    "url": "/Sushi-Gallery/static/js/2.1297564b.chunk.js"
+  },
+  {
+    "revision": "b1c357f8514a0f63ca2eca4cc25ee13c",
+    "url": "/Sushi-Gallery/static/js/2.1297564b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fc5b0afaa8077e6c57b",
+    "url": "/Sushi-Gallery/static/js/main.765ede66.chunk.js"
+  },
+  {
+    "revision": "ce1a3bf073f3cfbb0709",
+    "url": "/Sushi-Gallery/static/js/runtime-main.eb73472a.js"
+  },
+  {
+    "revision": "7fb0d49e82ef9b687f0fd0ec5467f8ba",
+    "url": "/Sushi-Gallery/static/media/background5.7fb0d49e.jpg"
+  },
+  {
+    "revision": "d8602e39bd2cbbde53a9654917daab92",
+    "url": "/Sushi-Gallery/static/media/brownies.d8602e39.otf"
+  }
+]);
